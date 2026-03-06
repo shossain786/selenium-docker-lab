efcbc0390a8a867ce38cca8276f49212941553a9
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2026-03-06 09:51:43 +0530

    base test updated

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -12,18 +12,27 @@ public class BaseTest {
     WebDriver driver;
     String appUrl = "https://panjatan.netlify.app/";
     boolean isRemote = false;
+    String remoteUrl = "http://localhost:4444";
 
     @BeforeEach
     public void setUp() throws MalformedURLException {
+        remoteUrl = System.getProperty("remoteUrl", "http://localhost:4444");
         isRemote  = System.getProperty("os.name").toLowerCase().contains("linux");
+        System.out.println("OS: " + System.getProperty("os.name") + ", isRemote: " + isRemote + ", remoteUrl: " + remoteUrl);
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
         options.addArguments("--disable-gpu");
 
-        driver = isRemote? new RemoteWebDriver(
-                new URL("http://localhost:4444"),
-                options
-        ) : new ChromeDriver(options);
+        if (isRemote) {
+            try {
+                driver = new RemoteWebDriver(new URL(remoteUrl), options);
+            } catch (Exception e) {
+                System.out.println("Remote WebDriver failed, falling back to local ChromeDriver: " + e.getMessage());
+                driver = new ChromeDriver(options);
+            }
+        } else {
+            driver = new ChromeDriver(options);
+        }
         driver.get(appUrl);
     }
     @AfterEach
