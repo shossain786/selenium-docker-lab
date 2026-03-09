caece5977c85c5b8a8204520b3a34c22e9018d73
Mohammad Saddam Hossain <sddmhossain786@gmail.com>
2026-03-09 08:28:43 +0530

    remote execution in parallel mode fixed

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,9 +1,12 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -17,28 +20,45 @@ public abstract class BaseTest {
         return driverThread.get();
     }
 
+    @Parameters("browser")
     @BeforeMethod
-    public void setUp() throws MalformedURLException {
+    public void setUp(String browser) throws MalformedURLException {
         String remoteUrl = System.getProperty("remoteUrl", "http://localhost:4444");
         boolean isRemote = Boolean.getBoolean("remote");
         System.out.println("OS: " + System.getProperty("os.name") + ", isRemote: " + isRemote + ", remoteUrl: " + remoteUrl);
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
 
         System.out.println("Remote Mode: " + isRemote);
-
         WebDriver driver;
-        if (isRemote) {
-            driver = new RemoteWebDriver(new URL(remoteUrl + "/wd/hub"), options);
+
+        if (browser.equalsIgnoreCase("firefox")) {
+
+            System.out.println("Firefox Browser");
+            FirefoxOptions options = new FirefoxOptions();
+
+            if (isRemote) {
+                driver = new RemoteWebDriver(new URL(remoteUrl + "/wd/hub"), options);
+            } else {
+                driver = new FirefoxDriver(options);
+            }
+
         } else {
-            driver = new ChromeDriver(options);
+            System.out.println("Chrome Browser");
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+
+            if (isRemote) {
+                driver = new RemoteWebDriver(new URL(remoteUrl + "/wd/hub"), options);
+            } else {
+                driver = new ChromeDriver(options);
+            }
         }
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         driverThread.set(driver);
-        driver.get(appUrl);
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        getDriver().get(appUrl);
     }
 
     @AfterMethod
